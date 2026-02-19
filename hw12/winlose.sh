ba1c8dfd2a71e525a1e38d92f636972c7817f52f
mcrockscaleb <calebcolorado@gmail.com>
2026-02-19 07:54:59 -0500

    hw12 on lab machine

--- hw12/winlose.sh
+++
@@ -1,8 +0,0 @@
-# Put your sed command (or commands) here
-# I'll get you started - this command changes lead into gold.
-# (You need to change it of course to be about win/lose stuff.)
-
-sed 's/lead/gold/g'
-
-# Remember you will probably need to do *multiple* sed's in a pipeline
-# to complete this HW!
